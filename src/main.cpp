439be21374c56af1b40edf595f9d4a973037566e
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2024-10-24 10:27:01 -0300

    Documentação

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
     Grafo g(0);
     int origem, destino;
 
-    g.carregarDeArquivo("bipartido2000.txt", origem, destino);
+    g.carregarDeArquivo("bipartido800.txt", origem, destino);
 
     auto inicio = chrono::high_resolution_clock::now();
     int fluxo = g.fluxoMaximo(origem, destino);
